4e60b81f59b2cf7fe9d20af62eb61421bf42f4f8
Lasse Lauwerys <lauweryslasse@gmail.com>
2025-07-30 22:32:29 +0200

    frarfe

--- CubeRendererWinUI/MainWindow.xaml.cpp
+++ CubeRendererWinUI/MainWindow.xaml.cpp
@@ -29,11 +29,20 @@ namespace winrt::CubeRendererWinUI::implementation
     ID2D1SolidColorBrush* d2dbrush;
     ID2D1Bitmap1* d2dTargetBitmap1;
     IDXGIDevice1* dxgiDevice;
+
+
+    ID3D11Device* device;
+
+    IDXGIDevice* dxgiDevice;
     //ID2D1DeviceContext* d2dContext;
     
+    void InitializeDirectX() {
+
+    }
+
     MainWindow::MainWindow()
     {
-        InitializeComponent();
+        //InitializeComponent();
 		// Xaml objects should not call InitializeComponent during construction.
 		// See
 
@@ -88,125 +97,244 @@ namespace winrt::CubeRendererWinUI::implementation
             auto nativePanel = swapChainPanel.as<ISwapChainPanelNative>();
             //nativePanel->SetSwapChain(swapChain);
 
-            swapChainPanel.SizeChanged([graphics](IInspectable const&, SizeChangedEventArgs const& e) {
-                try {
-                    auto size = e.NewSize();
-                    graphics->Resize(size.Width, size.Height);
-                    //graphics->Render(1.0f, -3.0f, -2.0f, 0.0f);
-                }
-                catch (const runtime_error& e) {
+            //swapChainPanel.SizeChanged([graphics](IInspectable const&, SizeChangedEventArgs const& e) {
+            //    try {
+            //        auto size = e.NewSize();
+            //        graphics->Resize(size.Width, size.Height);
+            //        //graphics->Render(1.0f, -3.0f, -2.0f, 0.0f);
+            //    }
+            //    catch (const runtime_error& e) {
 
-                }
-                });
+            //    }
+            //    });
 
             //graphics->Render(3.0f, -3.0f, -2.0f, 0.0f);
 
 
 
-			D2D1_FACTORY_OPTIONS options = {};
-			options.debugLevel = D2D1_DEBUG_LEVEL_INFORMATION;
-			D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, options, &d2dFactory);
+			//D2D1_FACTORY_OPTIONS options = {};
+			//options.debugLevel = D2D1_DEBUG_LEVEL_INFORMATION;
+			//D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, options, &d2dFactory);
 
-			ID3D11Device* d3dDevice = graphics->GetDevice();
+			//ID3D11Device* d3dDevice = graphics->GetDevice();
 
-			d3dDevice->QueryInterface(IID_PPV_ARGS(&dxgiDevice));
-             
-			d2dFactory->CreateDevice(dxgiDevice, &d2dDevice);
+			//d3dDevice->QueryInterface(IID_PPV_ARGS(&dxgiDevice));
+   //          
+			//d2dFactory->CreateDevice(dxgiDevice, &d2dDevice);
 
-			d2dDevice->CreateDeviceContext(D2D1_DEVICE_CONTEXT_OPTIONS_NONE, &d2dContext);
+			//d2dDevice->CreateDeviceContext(D2D1_DEVICE_CONTEXT_OPTIONS_NONE, &d2dContext);
 
-			IDXGIAdapter* dxgiAdapter = nullptr;
-			dxgiDevice->GetAdapter(&dxgiAdapter);
-			IDXGIFactory2* dxgiFactory = nullptr;
-			dxgiAdapter->GetParent(IID_PPV_ARGS(&dxgiFactory));
+			//IDXGIAdapter* dxgiAdapter = nullptr;
+			//dxgiDevice->GetAdapter(&dxgiAdapter);
+			//IDXGIFactory2* dxgiFactory = nullptr;
+			//dxgiAdapter->GetParent(IID_PPV_ARGS(&dxgiFactory));
 
-            DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
-            swapChainDesc.Width = 1000;
-            swapChainDesc.Height = 1000;
-            swapChainDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM; // this is the most common swapchain format
-            swapChainDesc.Stereo = false;
-            swapChainDesc.SampleDesc.Count = 1;                // don't use multi-sampling
-            swapChainDesc.SampleDesc.Quality = 0;
-            swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-            swapChainDesc.BufferCount = 2;                     // use double buffering to enable flip
-            swapChainDesc.Scaling = DXGI_SCALING_STRETCH;
-            swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL; // all apps must use this SwapEffect       
-            swapChainDesc.Flags = 0;
+   //         DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
+   //         swapChainDesc.Width = 1000;
+   //         swapChainDesc.Height = 1000;
+   //         swapChainDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM; // this is the most common swapchain format
+   //         swapChainDesc.Stereo = false;
+   //         swapChainDesc.SampleDesc.Count = 1;                // don't use multi-sampling
+   //         swapChainDesc.SampleDesc.Quality = 0;
+   //         swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+   //         swapChainDesc.BufferCount = 2;                     // use double buffering to enable flip
+   //         swapChainDesc.Scaling = DXGI_SCALING_STRETCH;
+   //         swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL; // all apps must use this SwapEffect       
+   //         swapChainDesc.Flags = 0;
 
-			IDXGISwapChain1* swapChain1 = nullptr;
-            dxgiFactory->CreateSwapChainForComposition(dxgiDevice, &swapChainDesc, NULL, &swapChain1);
+			//IDXGISwapChain1* swapChain1 = nullptr;
+   //         dxgiFactory->CreateSwapChainForComposition(dxgiDevice, &swapChainDesc, NULL, &swapChain1);
 
 
-            //dxgiFactory
-            D2D1_RENDER_TARGET_PROPERTIES renderTargetProperties = D2D1::RenderTargetProperties(
-                D2D1_RENDER_TARGET_TYPE_DEFAULT,
-                D2D1::PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_PREMULTIPLIED),
-				0, 0, D2D1_RENDER_TARGET_USAGE_NONE, D2D1_FEATURE_LEVEL_DEFAULT);
+   //         //dxgiFactory
+   //         D2D1_RENDER_TARGET_PROPERTIES renderTargetProperties = D2D1::RenderTargetProperties(
+   //             D2D1_RENDER_TARGET_TYPE_DEFAULT,
+   //             D2D1::PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_PREMULTIPLIED),
+			//	0, 0, D2D1_RENDER_TARGET_USAGE_NONE, D2D1_FEATURE_LEVEL_DEFAULT);
+   //         
+			//ID2D1RenderTarget* d2dRenderTarget = nullptr;
+			//IDXGISurface* dxgiSurface = nullptr;
+
+			////d2dFactory->CreateDxgiSurfaceRenderTarget(swapChain1, &renderTargetProperties, &d2dRenderTarget);
+   //         //d2dFactory->CreateDxgiSurfaceRenderTarget(dxgiSurface, &renderTargetProperties, &d2dRenderTarget);
+
+			//ID3D11Texture2D* backBuffer = nullptr;
+			//swapChain1->GetBuffer(0, IID_PPV_ARGS(&backBuffer));
+
+			//backBuffer->QueryInterface(IID_PPV_ARGS(&dxgiSurface));
+
+			//d2dContext->CreateBitmapFromDxgiSurface(dxgiSurface, 
+   //             D2D1::BitmapProperties1(
+   //                 D2D1_BITMAP_OPTIONS_TARGET | D2D1_BITMAP_OPTIONS_CANNOT_DRAW,
+   //                 D2D1::PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_PREMULTIPLIED)),
+			//	&d2dTargetBitmap1);
+
+   //         ID2D1SolidColorBrush* d2dBrush;
+   //         d2dContext->CreateSolidColorBrush(D2D1::ColorF(1.0f, 0.0f, 1.0f, 1.0f), &d2dBrush);
+
+			//d2dDevice->Release();
+			//dxgiDevice->Release();
+
+			//d2dContext->SetTarget(d2dTargetBitmap1);
+
+
+   //         swapChainPanel.SizeChanged([graphics, swapChain1, d2dBrush](IInspectable const&, SizeChangedEventArgs const& e) {
+   //             try {
+   //                 /*auto size = e.NewSize();
+   //                 graphics->Resize(size.Width, size.Height);*/
+   //                 //graphics->Render(1.0f, -3.0f, -2.0f, 0.0f);
+   //                 d2dContext->BeginDraw();
+   //         
+   //                 d2dContext->Clear(D2D1::ColorF(1.0f, 1.0f, 1.0f, 0.0f));
+
+			//        d2dContext->DrawLine(
+   //                     D2D1::Point2F(0.0f, 0.0f),
+   //                     D2D1::Point2F(100.0f, 100.0f),
+   //                     d2dBrush,
+			//	        1.0f);
+
+   //                 d2dContext->EndDraw();
+   //                 swapChain1->Present(1, 0);
+   //             }
+   //             catch (const runtime_error& e) {
+
+   //             }
+   //             });
+
+   //         nativePanel->SetSwapChain(swapChain1);
+
+
+   //         d2dContext->BeginDraw();
+
+   //         d2dContext->Clear(D2D1::ColorF(1.0f, 1.0f, 1.0f, 0.0f));
+
+   //         d2dContext->DrawLine(
+   //             D2D1::Point2F(0.0f, 0.0f),
+   //             D2D1::Point2F(100.0f, 100.0f),
+   //             d2dBrush,
+   //             1.0f);
+
+   //         d2dContext->EndDraw();
+   //         swapChain1->Present(1, 0);
+
+
+            D3D_FEATURE_LEVEL featureLevels[] = {
+                D3D_FEATURE_LEVEL_12_2,
+                D3D_FEATURE_LEVEL_12_1,
+                D3D_FEATURE_LEVEL_12_0,
+                D3D_FEATURE_LEVEL_11_1,
+                D3D_FEATURE_LEVEL_11_0,
+                D3D_FEATURE_LEVEL_10_1,
+                D3D_FEATURE_LEVEL_10_0,
+                D3D_FEATURE_LEVEL_9_3,
+                D3D_FEATURE_LEVEL_9_2,
+                D3D_FEATURE_LEVEL_9_1
+            };
+
+            ID3D11DeviceContext* tempContext;
+
+            D3D11CreateDevice(
+                NULL,
+                D3D_DRIVER_TYPE_HARDWARE, 0,
+                D3D11_CREATE_DEVICE_BGRA_SUPPORT | D3D11_CREATE_DEVICE_DEBUG,
+                featureLevels, ARRAYSIZE(featureLevels), D3D11_SDK_VERSION, &device, NULL, &tempContext);
             
-			ID2D1RenderTarget* d2dRenderTarget = nullptr;
-			IDXGISurface* dxgiSurface = nullptr;
 
-			//d2dFactory->CreateDxgiSurfaceRenderTarget(swapChain1, &renderTargetProperties, &d2dRenderTarget);
-            //d2dFactory->CreateDxgiSurfaceRenderTarget(dxgiSurface, &renderTargetProperties, &d2dRenderTarget);
+            // deviceContext = tempContext;
+            
+            device->QueryInterface(IID_PPV_ARGS(&dxgiDevice));
 
-			ID3D11Texture2D* backBuffer = nullptr;
-			swapChain1->GetBuffer(0, IID_PPV_ARGS(&backBuffer));
+            
 
-			backBuffer->QueryInterface(IID_PPV_ARGS(&dxgiSurface));
-
-			d2dContext->CreateBitmapFromDxgiSurface(dxgiSurface, 
-                D2D1::BitmapProperties1(
-                    D2D1_BITMAP_OPTIONS_TARGET | D2D1_BITMAP_OPTIONS_CANNOT_DRAW,
-                    D2D1::PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_PREMULTIPLIED)),
-				&d2dTargetBitmap1);
-
-            ID2D1SolidColorBrush* d2dBrush;
-            d2dContext->CreateSolidColorBrush(D2D1::ColorF(1.0f, 0.0f, 1.0f, 1.0f), &d2dBrush);
-
-			d2dDevice->Release();
-			dxgiDevice->Release();
-
-			d2dContext->SetTarget(d2dTargetBitmap1);
-
-
-            swapChainPanel.SizeChanged([graphics, swapChain1, d2dBrush](IInspectable const&, SizeChangedEventArgs const& e) {
+            swapChainPanel.Loaded([&, dxgiDevice, device](IInspectable const&, RoutedEventArgs const&) {
                 try {
-                    /*auto size = e.NewSize();
-                    graphics->Resize(size.Width, size.Height);*/
-                    //graphics->Render(1.0f, -3.0f, -2.0f, 0.0f);
-                    d2dContext->BeginDraw();
-            
-                    d2dContext->Clear(D2D1::ColorF(1.0f, 1.0f, 1.0f, 0.0f));
 
-			        d2dContext->DrawLine(
-                        D2D1::Point2F(0.0f, 0.0f),
-                        D2D1::Point2F(100.0f, 100.0f),
-                        d2dBrush,
-				        1.0f);
+                    auto swapChainPanel = SwapChainPanel();
+                    auto nativePanel = swapChainPanel.as<ISwapChainPanelNative>();
 
-                    d2dContext->EndDraw();
-                    swapChain1->Present(1, 0);
+                    DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
+                    swapChainDesc.Stereo = FALSE;
+					swapChainDesc.Width = (int)swapChainPanel.Width();
+					swapChainDesc.Height = (int)swapChainPanel.Height();
+					swapChainDesc.BufferCount = 2;
+					swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+					swapChainDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM; // this is the most common swapchain format
+					swapChainDesc.SampleDesc.Count = 1; // don't use multi-sampling
+					swapChainDesc.SampleDesc.Quality = 0;
+					swapChainDesc.Scaling = DXGI_SCALING_STRETCH;
+					swapChainDesc.AlphaMode = DXGI_ALPHA_MODE_PREMULTIPLIED;
+					swapChainDesc.Flags = 0;
+					swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL; // all apps must use this SwapEffect
+
+                    IDXGIAdapter1* dxgiAdapter;
+                    dxgiDevice->GetParent(IID_PPV_ARGS(&dxgiAdapter));
+                    IDXGIFactory2* dxgiFactory2;
+                    dxgiAdapter->GetParent(IID_PPV_ARGS(&dxgiFactory2));
+
+                    IDXGISwapChain1* swapChain;
+
+                    // resize window flick bug
+                    dxgiFactory2->CreateSwapChainForComposition(device, &swapChainDesc, NULL, &swapChain);
+
+                    ID3D11Texture2D* backBuffer;
+                    swapChain->GetBuffer(0, IID_PPV_ARGS(&backBuffer));
+                    // renderTargetView = device.CreateRenderTargetView(backBuffer);
+
+                    IDXGISurface* dxgiBackBuffer;
+                    backBuffer->QueryInterface(IID_PPV_ARGS(&dxgiBackBuffer));
+
+                    nativePanel->SetSwapChain(swapChain);
+
+                    ID2D1Factory1* d2dFactory;
+                    D2D1CreateFactory(D2D1_FACTORY_TYPE_MULTI_THREADED, IID_PPV_ARGS(&d2dFactory));
+
+                    d2dFactory->CreateDevice(dxgiDevice, &d2dDevice);
+                    d2dDevice->CreateDeviceContext(D2D1_DEVICE_CONTEXT_OPTIONS_NONE, &d2dContext);
+
+					auto bitmapProperties = D2D1::BitmapProperties1(
+                        D2D1_BITMAP_OPTIONS_TARGET | D2D1_BITMAP_OPTIONS_CANNOT_DRAW,
+                        D2D1::PixelFormat(
+                            DXGI_FORMAT_B8G8R8A8_UNORM,
+							D2D1_ALPHA_MODE_PREMULTIPLIED), 144, 144);
+
+                    //bitmapProperties.PixelFormat.Format = Format.B8G8R8A8_UNorm;
+                    //bitmapProperties.PixelFormat.AlphaMode = Vortice.DCommon.AlphaMode.Premultiplied;
+                    //bitmapProperties.BitmapOptions = BitmapOptions.Target | BitmapOptions.CannotDraw;
+                    //bitmapProperties.DpiX = 144;
+                    //bitmapProperties.DpiY = 144;
+                    d2dContext->CreateBitmapFromDxgiSurface(dxgiBackBuffer, bitmapProperties, &d2dTargetBitmap1);
+                    d2dContext->SetTarget(d2dTargetBitmap1);
+                    d2dContext->CreateSolidColorBrush(D2D1::ColorF(0.0f, 0.0f, 1.0f, 1.0f), &d2dbrush);
+
+                    dxgiDevice->Release();
+
+                    swapChainPanel.SizeChanged([swapChain](IInspectable const&, SizeChangedEventArgs const& e) {
+                        try {
+                            /*auto size = e.NewSize();
+                            graphics->Resize(size.Width, size.Height);*/
+                            //graphics->Render(1.0f, -3.0f, -2.0f, 0.0f);
+                            d2dContext->BeginDraw();
+
+                            d2dContext->Clear(D2D1::ColorF(1.0f, 1.0f, 1.0f, 0.0f));
+
+                            d2dContext->DrawLine(
+                                D2D1::Point2F(0.0f, 0.0f),
+                                D2D1::Point2F(100.0f, 100.0f),
+                                d2dbrush,
+                                1.0f);
+
+                            d2dContext->EndDraw();
+                            swapChain->Present(1, 0);
+                        }
+                        catch (const runtime_error& e) {
+
+                        }
+                        });
                 }
-                catch (const runtime_error& e) {
-
+                catch (const hresult_error& e) {
+                    OutputDebugString(e.message().c_str());
                 }
-                });
-
-            nativePanel->SetSwapChain(swapChain1);
-
-
-            d2dContext->BeginDraw();
-
-            d2dContext->Clear(D2D1::ColorF(1.0f, 1.0f, 1.0f, 0.0f));
-
-            d2dContext->DrawLine(
-                D2D1::Point2F(0.0f, 0.0f),
-                D2D1::Point2F(100.0f, 100.0f),
-                d2dBrush,
-                1.0f);
-
-            d2dContext->EndDraw();
-            swapChain1->Present(1, 0);
+				});
         }
         catch (const runtime_error& e) {
 
