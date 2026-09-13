64f37f9e931c2a839770ce50620b16fafec07b41
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-09-13 21:45:21 +0200

    uh restore the file wt

--- include/Scene.h
+++ include/Scene.h
@@ -2,16 +2,59 @@
 
 //#include <functional>
 
-//#include <Windows.h>
-
 #include "Base.h"
 
 #include "Texture.h"
-
 //#include <Windows.h>
 
 namespace CubeRenderer {
-struct Vertex {
-	 struct { float x, y, z; } position;
-	 struct { float u, v; } textureCoordinate;
+	struct Vertex {
+		struct { float x, y, z; } position;
+		struct { float u, v; } textureCoordinate;
+		float blur;
 	};
+
+
+	class Scene
+	{
+	private:
+		size_t verticesSize = 0;
+		Vertex* vertices;
+
+		UINT indexCount;
+		size_t indicesSize = 0;
+		USHORT* indices;
+
+		UINT cubeCount = 0;
+
+		Texture* texture;
+
+		Vertex* CreateVertices(float width, float height, float depth, float offsetX, float offsetY, float offsetZ, float u, float v, Texture* texture, size_t* sizeOut, UINT* countOut);
+		USHORT* CreateIndices(USHORT offset, size_t* sizeOut, UINT* countOut);
+
+		inline void ThrowIfFailed(HRESULT hr)
+		{
+			if (FAILED(hr))
+			{
+				if (OnError) OnError(hr);
+			}
+		}
+	public:
+		Scene();
+		~Scene();
+
+		Scene(const Scene&) = delete;
+		Scene& operator=(const Scene&) = delete;
+		UINT GetIndexCount();
+		size_t GetIndicesSize();
+		size_t GetVerticesSize();
+		Vertex* GetVertices();
+		USHORT* GetIndices();
+		void AddCube(float width, float height, float depth, float x, float y, float z, float u, float v, Texture* texture);
+
+		void SetTexture(Texture* texture);
+		Texture* GetTexture();
+
+		std::function<void(HRESULT)> OnError;
+	};
+}
